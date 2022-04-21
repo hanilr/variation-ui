3f64d7b233a7d403d9ffb49669bae66e6b0b6b07
hanilr <hakkianilragiboglu@gmail.com>
2022-04-21 21:32:50 +0300

    A little change to 'vn_list' function and change of some comments

--- src/lib/vn_widget.h
+++ src/lib/vn_widget.h
@@ -4,12 +4,12 @@
 
 void vn_bg(int pos_x, int pos_y, int width, int height, char *bg_color); /* SET BACKGROUND COLOR */
 
-void vn_frame(int pos_x, int pos_y, int width, int height, char *fg_color, char *bg_color, char vertical_symbol, char horizontal_symbol); /* SET FRAME */
+void vn_frame(int pos_x, int pos_y, int width, int height, char *fg_color, char *bg_color, char vertical_symbol, char horizontal_symbol); /* SET WINDOW FRAME */
 
 void vn_label(int pos_x, int pos_y, int width, int height, char *fg_color, char *bg_color, char* text_style, char *str); /* SET LABEL */
 
-void vn_progress(int pos_x, int pos_y, int width, int height, char *progress_frame_color, char *progress_color, int progress_value); /* PROGRESS BAR */
+void vn_progress(int pos_x, int pos_y, int width, int height, char *progress_frame_color, char *progress_color, int progress_value); /* SET PROGRESS BAR */
 
-void vn_list(int pos_x, int pos_y, int width, int height, char *list_bg_color, char *list_fg_color, char *list_title, char *list_content); /* LIST LABEL */
+void vn_list(int pos_x, int pos_y, int width, int height, char *list_fg_color, char *list_bg_color, char *list_title, char *list_content); /* SET LIST LABEL */
 
 #endif /* MADE BY @hanilr */
